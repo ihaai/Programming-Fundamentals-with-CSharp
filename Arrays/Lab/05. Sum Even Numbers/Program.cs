9c5d5bfeff1141689e766a634259cf0ee235888b
ihaai <69941441+ihaai@users.noreply.github.com>
2021-09-29 08:52:24 -0700

    Wednesday, 29 September

---
+++ Arrays/Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int summator = 0;
+            for (int i = 0; i <= numArray.Length-1; i++)
+            {
+                if (numArray[i] % 2 == 0)
+                {
+                    summator += numArray[i];
+                }
+            }
+            Console.WriteLine(summator);
+        }
+    }
+}
