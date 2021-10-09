cd5e16a9579f4a2bb61274d9d08f2434bbf4c806
ihaai <69941441+ihaai@users.noreply.github.com>
2021-10-09 12:08:32 -0700

    Friday, October 1 - Fixed 06

--- Arrays/Exercise/06. Equal Sum/Program.cs
+++ Arrays/Exercise/06. Equal Sum/Program.cs
@@ -7,50 +7,34 @@ namespace _06._Equal_Sum
     {
         static void Main(string[] args)
         {
-            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int[] leftArr = numbers.Take(numbers.Length / 2).ToArray();
-            int[] rightArr = numbers.Skip(numbers.Length / 2).ToArray();
+            int[] integerArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int leftSum = new int();
+            int rightSum = new int();
 
+            for (int mainIndex = 0; mainIndex < integerArray.Length; mainIndex++)
+            {
+                leftSum = 0;
+                rightSum = 0;
 
-            //int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            //int LSUM = 0;
-            //int RSUM = 0;
+                // Left side sum loop
+                for (int left = 0; left < mainIndex; left++)
+                {
+                    leftSum += integerArray[left];
+                }
 
-            //for (int i = 0; i < numbers.Length; i++)
-            //{
-            //    if (numbers.Length == 1)
-            //    {
-            //        Console.WriteLine(0);
-            //        return;
-            //    }
-            //    LSUM = 0;
-            //    for (int sumLeft = i; sumLeft > 0; sumLeft--)
-            //    {
-            //        int nextElementPos = sumLeft - 1;
-            //        if (sumLeft > 0)
-            //        {
-            //            LSUM += numbers[nextElementPos];
-            //        }
-            //    }
+                // Right side sum loop
+                for (int right = integerArray.Length - 1; right > mainIndex; right--)
+                {
+                    rightSum += integerArray[right];
+                }
 
-            //    RSUM = 0;
-            //    for (int j = i; j < numbers.Length; j++)
-            //    {
-            //        int nextElementPos = j + 1;
-            //        if (j < numbers.Length - 1)
-            //        {
-            //            RSUM += numbers[nextElementPos];
-            //        }
-            //    }
-
-            //    if (RSUM == LSUM)
-            //    {
-            //        Console.WriteLine(i);
-            //        return;
-            //    }
-            //}
-
-            //Console.WriteLine("no");
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(mainIndex);
+                    return; // Stopping the program.
+                }
+            }
+            Console.WriteLine("no");
         }
     }
 }
