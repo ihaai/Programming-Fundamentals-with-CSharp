6eaa5c13f4d932c1184b63f3130c658f3f69aca9
ihaai <69941441+ihaai@users.noreply.github.com>
2021-10-09 12:04:02 -0700

    Optional exercises - Fixed 03

--- Arrays/More Exercise/03. Recursive Fibonacci/Program.cs
+++ Arrays/More Exercise/03. Recursive Fibonacci/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _03._Recursive_Fibonacci
 {
     class Program
     {
+
+        static long Fibanocci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            long Fn = Fibanocci(n - 1) + Fibanocci(n - 2); // <== Following the formula.
+            return Fn;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fibanocciStorage = new List<long>();
+
+            int endRange = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= endRange; i++)
+            {
+                fibanocciStorage.Add(Fibanocci(i));
+            }
+
+            Console.WriteLine(fibanocciStorage.Last());
         }
     }
 }
