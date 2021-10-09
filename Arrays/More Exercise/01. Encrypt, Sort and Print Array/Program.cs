b2dc83a6980f5d8ba5e1721d3d2ec32c84100f19
ihaai <69941441+ihaai@users.noreply.github.com>
2021-10-09 11:56:01 -0700

    Optional exercises

---
+++ Arrays/More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLinesToRead = int.Parse(Console.ReadLine());
+            int[] wordsToEncrypt = new int[numberOfLinesToRead];
+            char[] vowels = { 'a', 'A', 'o', 'O', 'e', 'E', 'i', 'I', 'u', 'U' };
+
+            for (int i = 0; i < numberOfLinesToRead; i++)
+            {
+                string stringInput = Console.ReadLine();
+                char[] inputToChar = stringInput.ToCharArray();
+                int sums = 0;
+
+                for (int a = 0; a < inputToChar.Length; a++)
+                {
+                    if (vowels.Contains(inputToChar[a]))
+                    {
+                        sums += inputToChar[a] * stringInput.Length;
+                    }
+                    else
+                    {
+                        sums += inputToChar[a] / stringInput.Length;
+                    }
+                }
+                wordsToEncrypt[i] = sums;
+            }
+            Array.Sort(wordsToEncrypt);
+            foreach (var element in wordsToEncrypt)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ Arrays/More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int triangleHeight = int.Parse(Console.ReadLine());
+            long numberToPrintInTriangle = new long();
+            for (long row = 0; row < triangleHeight; row++)
+            {
+                numberToPrintInTriangle = 1;
+                for (long col = 0; col<= row; col++)
+                {
+                    Console.Write(numberToPrintInTriangle + " ");
+                    numberToPrintInTriangle 
+                        = numberToPrintInTriangle * (row - col) / (col + 1);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
