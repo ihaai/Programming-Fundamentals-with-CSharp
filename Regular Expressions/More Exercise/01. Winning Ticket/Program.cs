a63492717bd37271d0c2a6b41c00f2e396ffc8a0
ihaai <69941441+ihaai@users.noreply.github.com>
2021-11-23 01:26:36 +0300

    Optional exercises 2/4

---
+++ Regular Expressions/More Exercise/01. Winning Ticket/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Winning_Ticket
+{
+    public class WinTicket
+    {
+        Regex pattern = new Regex(@"(\${6,10}|\@{6,10}|\#{6,10}|\^{6,10})");
+        private string left;
+        private string right;
+        public Match match;
+
+        public bool ValidateTicket(string ticket)
+        {
+            if (ticket.Length != 20)
+                return false;
+            return true;
+        }
+
+        public bool RegexValidation(string ticket)
+        {
+            if (pattern.IsMatch(ticket))
+            {
+                match = pattern.Match(ticket);
+                return true;
+            }
+            return false;
+        }
+
+        public string GetLeftSymbols(string ticket)
+        {
+            left = pattern.Match(string.Join("", ticket.Take(10))).Value;
+            return left;
+        }
+
+        public string GetRightSymbols(string ticket)
+        {
+            right = pattern.Match(string.Join("", ticket.Skip(10).Take(10))).Value;
+            return right;
+        }
+
+        public Match GetMatch(string ticket) => pattern.Match(ticket);
+
+        public int LeftSymbolCount(string ticket) => left.Count(x => x == match.Value[0]);
+
+        public int RightSymbolCount(string ticket) => right.Count(x => x == match.Value[0]);
+
+        public string NoMatch(string ticket) => $"ticket \"{ticket}\" - no match";
+
+        public string Invalid(string ticket) => "invalid ticket";
+
+        public string Jackpot(string ticket, int min, int symbol) => $"ticket \"{ticket}\" - {min}{Char.ConvertFromUtf32(symbol)} Jackpot!";
+
+        public string NormalWin(string ticket, int min, int symbol) => $"ticket \"{ticket}\" - {min}{Char.ConvertFromUtf32(symbol)}";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WinTicket ticket = new WinTicket();
+            StringBuilder sb = new StringBuilder();
+            List<string> input = Console.ReadLine().Trim().Split(new[] { ',', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (ticket.ValidateTicket(input[i]))
+                {
+                    if (ticket.RegexValidation(input[i]))
+                    {
+                        Match match = ticket.match;
+                        string left = ticket.GetLeftSymbols(input[i]);
+                        string right = ticket.GetRightSymbols(input[i]);
+                        int leftCount = ticket.LeftSymbolCount(input[i]);
+                        int rightCount = ticket.RightSymbolCount(input[i]);
+                        int min = Math.Min(leftCount, rightCount);
+
+                        if (min == 10)
+                        {
+                            Console.WriteLine(ticket.Jackpot(input[i], min, match.Value[0]));
+                        }
+                        else if (leftCount == 0 || rightCount == 0)
+                        {
+                            Console.WriteLine(ticket.NoMatch(input[i]));
+                        }
+                        else
+                        {
+                            Console.WriteLine(ticket.NormalWin(input[i], min, match.Value[0]));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(ticket.NoMatch(input[i]));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(ticket.Invalid(input[i]));
+                }
+            }
+        }
+    }
+}
---
+++ Regular Expressions/More Exercise/02. Rage Quit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Rage_Quit
+{
+    public class Rage
+    {
+        public StringBuilder sb = new StringBuilder();
+        Regex numbers = new Regex(@"\d+");
+        Regex chars = new Regex(@"[^\d]+");
+
+        public List<byte> GetNumbers(string input) => numbers.Matches(input).Select(x => byte.Parse(x.Value)).ToList();
+
+        public List<string> GetChars(string input) => chars.Matches(input).Select(x => x.Value).ToList();
+
+        public int ReturnQuiqueChars() => sb.ToString().Distinct().Count();
+
+        public void RepeatedAppend(string input, List<string> chars, List<byte> nums)
+        {
+            for (int i = 0; i < chars.Count; i++)
+            {
+                var repeatedChar = Enumerable.Repeat(chars[i], nums[i]);
+
+                foreach (var element in repeatedChar)
+                {
+                    sb.Append(element);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rage quit = new Rage();
+
+            string input = Console.ReadLine().ToUpper();
+            List<byte> numCollection = quit.GetNumbers(input);
+            List<string> charsCollection = quit.GetChars(input);
+            quit.RepeatedAppend(input, charsCollection, numCollection);
+
+            Console.WriteLine($"Unique symbols used: {quit.ReturnQuiqueChars()}");
+            Console.WriteLine(quit.sb.ToString());
+        }
+    }
+}
