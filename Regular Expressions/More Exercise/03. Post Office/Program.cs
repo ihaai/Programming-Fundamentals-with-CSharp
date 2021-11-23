62d57a2dfb13a0cd95f5ffcce2412c599fd2c68c
ihaai <69941441+ihaai@users.noreply.github.com>
2021-11-23 22:01:33 +0300

    Optional exercises

---
+++ Regular Expressions/More Exercise/03. Post Office/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _3._Post_Office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex firstPattern = new Regex(@"(\$|\%|\#|\*|\&)(?<capitals>[A-Z]+)\1");
+
+            List<string> cryptedAscii = Console.ReadLine().Split('|').ToList();
+            string first = cryptedAscii[0];
+            string second = cryptedAscii[1];
+            string third = cryptedAscii[2];
+
+            first = firstPattern.Match(first).Groups["capitals"].Value;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                int asciiCode = (char)first[i];
+                Regex secondPattern = new Regex($@"({asciiCode}):(?<lenght>[0-9][0-9])");
+                int lenght = int.Parse(secondPattern.Match(second).Groups["lenght"].Value);
+
+                Regex thirdPattern = new Regex($@"(?<!\S){first[i]}.{{{lenght}}}(?!\S)");
+                string word = thirdPattern.Match(third).Value;
+
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Regular Expressions/More Exercise/04. Santa's Secret Helper/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _4._Santa_s_Secret_Helper
+{
+    public class Helper
+    {
+        public List<string> goodNames = new List<string>();
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public Helper() { }
+
+        public Helper(string name, string type)
+        {
+            this.Name = name;
+            this.Type = type;
+        }
+
+        public void CheckIfHelperIsGood(Helper helper)
+        {
+            if (helper.Type == "G")
+                goodNames.Add(helper.Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"@(?<name>[A-Za-z]+)[^\@\-\!\:\>]*\!(?<bType>[GN])\!");
+            StringBuilder decrypted = new StringBuilder();
+            Helper classInstance = new Helper();
+
+            int key = int.Parse(Console.ReadLine());
+            string crypted = string.Empty;
+
+            while ((crypted = Console.ReadLine()) != "end")
+            {
+                foreach (var curChar in crypted)
+                    decrypted.Append(Char.ConvertFromUtf32((char)curChar - key));
+
+                string name = pattern.Match(decrypted.ToString()).Groups["name"].Value;
+                string type = pattern.Match(decrypted.ToString()).Groups["bType"].Value;
+
+                classInstance.CheckIfHelperIsGood(new Helper(name, type));
+
+                decrypted.Clear();
+            }
+            Console.WriteLine(string.Join("\n", classInstance.goodNames));
+        }
+    }
+}
